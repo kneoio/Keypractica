734a203954c695cb9291dcfae27347ac16a22801
kaira <biosubj@gmail.com>
2023-07-02 20:13:17 +0300

    #

--- src/main/java/com/semantyca/model/DataEntity.java
+++ src/main/java/com/semantyca/model/DataEntity.java
@@ -1,5 +1,10 @@
 package com.semantyca.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.semantyca.util.CustomLocalDateTimeDeserializer;
+import com.semantyca.util.CustomLocalDateTimeSerializer;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,12 +16,17 @@ import java.util.Map;
 @Getter
 public abstract class DataEntity<K> implements IDataEntity<K> {
     protected long author;
+    @JsonDeserialize(using = CustomLocalDateTimeDeserializer.class)
+    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
     private LocalDateTime regDate;
+    @JsonDeserialize(using = CustomLocalDateTimeDeserializer.class)
+    @JsonSerialize(using = CustomLocalDateTimeSerializer.class)
     private LocalDateTime lastModifiedDate;
     private long lastModifier;
     private boolean editable;
     private String title = "";
 
+    @JsonIgnore
     public Map<String, String> getValuesAsMap() {
         Map<String, String> values = new HashMap<>();
         values.put("author", String.valueOf(author));
--- src/main/java/com/semantyca/server/MapperCustomizer.java
+++ src/main/java/com/semantyca/server/MapperCustomizer.java
@@ -3,15 +3,28 @@ package com.semantyca.server;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.semantyca.util.CustomLocalDateTimeDeserializer;
+import com.semantyca.util.CustomLocalDateTimeSerializer;
 import io.quarkus.jackson.ObjectMapperCustomizer;
-import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Singleton;
+
+import java.time.LocalDateTime;
 
 
-@ApplicationScoped
+@Singleton
 public class MapperCustomizer implements ObjectMapperCustomizer {
+
+//TODO make it able to register (to eliminate the field annotation)
+
     @Override
     public void customize(ObjectMapper objectMapper) {
+        System.out.println("##################################################################regiater");
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        SimpleModule module = new SimpleModule();
+        module.addDeserializer(LocalDateTime.class, new CustomLocalDateTimeDeserializer());
+        module.addSerializer(LocalDateTime.class, new CustomLocalDateTimeSerializer());
+        objectMapper.registerModule(module);
     }
 }
