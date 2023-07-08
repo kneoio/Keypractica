2d6a88f85f51d1f7ed510006d0981dab69e17c53
kaira <biosubj@gmail.com>
2023-07-08 17:16:18 +0300

    #

--- src/main/java/com/semantyca/server/ApplicationInit.java
+++ src/main/java/com/semantyca/server/ApplicationInit.java
@@ -2,8 +2,12 @@ package com.semantyca.server;
 
 import io.quarkus.runtime.ShutdownEvent;
 import io.quarkus.runtime.StartupEvent;
+import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.pgclient.PgPool;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,11 +17,27 @@ public class ApplicationInit {
 
     private static final Logger LOGGER = LoggerFactory.getLogger("ListenerBean");
 
+    @Inject
+    @ConfigProperty(name = "quarkus.datasource.reactive.url")
+    String jdbcUrl;
 
+    @Inject
+    PgPool client;
 
     void onStart(@Observes StartupEvent ev)  {
         LOGGER.info("The application is starting...{}", EnvConst.APP_ID);
-        initdb();
+        if (EnvConst.DEV_MODE) {
+            LOGGER.info(EnvConst.APP_ID + "'s dev mode enabled");
+            LOGGER.info("Database: {}", jdbcUrl);
+            Uni<String> connected = client.query("SELECT 1")
+                    .execute()
+                    .onItem()
+                    .transform(rows ->  "Database connected ...")
+                    .onFailure()
+                    .recoverWithItem("Database connection failed");
+            LOGGER.info(connected.await().indefinitely());
+
+        }
 
     }
 
@@ -25,17 +45,6 @@ public class ApplicationInit {
         LOGGER.info("The application is stopping...");
     }
 
-
-    private void initdb() {
-        /*client.query("DROP TABLE IF EXISTS fruits").execute()
-                .flatMap(r -> client.query("CREATE TABLE fruits (id SERIAL PRIMARY KEY, name TEXT NOT NULL)").execute())
-                .flatMap(r -> client.query("INSERT INTO fruits (name) VALUES ('Kiwi')").execute())
-                .flatMap(r -> client.query("INSERT INTO fruits (name) VALUES ('Durian')").execute())
-                .flatMap(r -> client.query("INSERT INTO fruits (name) VALUES ('Pomelo')").execute())
-                .flatMap(r -> client.query("INSERT INTO fruits (name) VALUES ('Lychee')").execute())
-                .await().indefinitely();*/
-        //CREATE CONSTRAINT unique_language_code FOR (n:Language) REQUIRE n.code IS UNIQUE
-    }
 
 
 }
