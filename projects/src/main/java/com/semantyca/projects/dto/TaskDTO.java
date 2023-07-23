1b13b9a70d173cbccbf5035e60015cf5726afdbb
kaira <biosubj@gmail.com>
2023-07-23 12:14:56 +0300

    #

--- projects/src/main/java/com/semantyca/projects/dto/TaskDTO.java
+++ projects/src/main/java/com/semantyca/projects/dto/TaskDTO.java
@@ -2,8 +2,8 @@ package com.semantyca.projects.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.semantyca.core.dto.IDTO;
+import com.semantyca.core.dto.rls.RLSDTO;
 import com.semantyca.core.localization.LanguageCode;
-import com.semantyca.core.model.embedded.RLS;
 import com.semantyca.projects.model.Project;
 import com.semantyca.projects.model.Task;
 
@@ -13,6 +13,6 @@ import java.util.UUID;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 //public record TaskDTO(UUID id, String regNumber, String body, ZonedDateTime startDate, ZonedDateTime targetDate) {
-public record TaskDTO(UUID id, String regNumber, String body, String assignee, java.util.Map<LanguageCode, String> taskType, Project project, Task parent, ZonedDateTime startDate, ZonedDateTime targetDate, int status , int priority, List<RLS> rls) implements IDTO {
+public record TaskDTO(UUID id, String regNumber, String body, String assignee, java.util.Map<LanguageCode, String> taskType, Project project, Task parent, ZonedDateTime startDate, ZonedDateTime targetDate, int status , int priority, List<RLSDTO> rls) implements IDTO {
 
 }
