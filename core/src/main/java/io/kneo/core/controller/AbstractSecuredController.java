a0ac493f6c966f34d2f3b480d9afcb6508649095
Aida <justaidajam@gmail.com>
2023-11-29 18:24:23 +0200

    #

--- core/src/main/java/io/kneo/core/controller/AbstractSecuredController.java
+++ core/src/main/java/io/kneo/core/controller/AbstractSecuredController.java
@@ -10,8 +10,16 @@ public abstract class AbstractSecuredController<T, V> extends AbstractController
     protected static final String UUID_PATTERN = "^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$";
     private static final String USER_NAME_CLAIM = "preferred_username";
     protected Optional<IUser> getUserId(ContainerRequestContext requestContext) {
-        DefaultJWTCallerPrincipal securityIdentity = (DefaultJWTCallerPrincipal) requestContext.getSecurityContext().getUserPrincipal();
-        return userService.findByLogin(securityIdentity.getClaim(USER_NAME_CLAIM));
+        try {
+            DefaultJWTCallerPrincipal securityIdentity = (DefaultJWTCallerPrincipal) requestContext.getSecurityContext().getUserPrincipal();
+            return userService.findByLogin(securityIdentity.getClaim(USER_NAME_CLAIM));
+        } catch (NullPointerException e) {
+            LOGGER.warn(String.format("msg: %s ", e.getMessage()));
+            return Optional.empty();
+        } catch (Exception e) {
+            LOGGER.error(String.format("msg: %s ", e.getMessage()), e);
+            return Optional.empty();
+        }
     }
 
     protected String getUserOIDCName(ContainerRequestContext requestContext) {
