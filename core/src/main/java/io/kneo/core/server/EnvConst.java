0984bf796891a0f1020f9c028d9831a58d349a10
aida <aidazimas@hotmail.com>
2024-05-06 10:08:34 +0100

    #

--- core/src/main/java/io/kneo/core/server/EnvConst.java
+++ core/src/main/java/io/kneo/core/server/EnvConst.java
@@ -3,7 +3,7 @@ package io.kneo.core.server;
 public class EnvConst {
     public static final int DEFAULT_PAGE_SIZE = 20;
     public static final String APP_ID = "Keypractica" ;
-    public static final String VERSION = "1.0" ;
+    public static final String VERSION = "1.1" ;
     public static final String PATH_TO_LICENSE = "/legal" ;
     public static final int DEFAULT_HTTP_PORT = 80;
     public static boolean DEV_MODE = true;
@@ -11,6 +11,6 @@ public class EnvConst {
     public static String DEFAULT_DATETIME_FORMAT = "dd.MM.yyyy kk:mm";
     public static String DEFAULT_DATE_FORMAT = "dd.MM.yyyy";
 
-    public static String DEFAULT_COUNTRY_OF_NUMBER_FORMAT = "lv";
+    public static String DEFAULT_COUNTRY_OF_NUMBER_FORMAT = "pt";
 
 }
