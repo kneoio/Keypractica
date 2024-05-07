5716d8695025c5ea58c9b77d0ab1fb3290e63c9a
aida <aidazimas@hotmail.com>
2024-05-07 22:06:17 +0100

    #

--- core/src/main/java/io/kneo/core/server/EnvConst.java
+++ core/src/main/java/io/kneo/core/server/EnvConst.java
@@ -3,7 +3,7 @@ package io.kneo.core.server;
 public class EnvConst {
     public static final int DEFAULT_PAGE_SIZE = 20;
     public static final String APP_ID = "Keypractica" ;
-    public static final String VERSION = "1.1.1" ;
+    public static final String VERSION = "1.1.2" ;
     public static final String PATH_TO_LICENSE = "/legal" ;
     public static final int DEFAULT_HTTP_PORT = 80;
     public static boolean DEV_MODE = true;
