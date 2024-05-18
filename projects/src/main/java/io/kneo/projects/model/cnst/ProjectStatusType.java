3d70af9d04fc8be7e9be58d9e7dd4bdb7f322ec4
aida <aidazimas@hotmail.com>
2024-05-18 23:05:17 +0100

    Refactor ProjectStatusType enum and add new type
    The ProjectStatusType enum in ProjectStatusType.java has been refactored for better readability. The 'Getter' annotation from Lombok library has been used to replace the existing getter method. In addition, a new deprecated enumeration type 'COMPLETE' has been added to the list.

--- projects/src/main/java/io/kneo/projects/model/cnst/ProjectStatusType.java
+++ projects/src/main/java/io/kneo/projects/model/cnst/ProjectStatusType.java
@@ -1,19 +1,26 @@
 package io.kneo.projects.model.cnst;
 
+import lombok.Getter;
+
 /**
  * @author Kayra created 21-04-2016
  */
+@Getter
 public enum ProjectStatusType {
-    UNKNOWN(0), DRAFT(899), @Deprecated PROCESSED(900), COMPLETED(901), @Deprecated PROCESSING(902), ACTIVE(903), MERGED(904), PAUSED(910);
+    UNKNOWN(0),
+    DRAFT(899),
+    @Deprecated PROCESSED(900),
+    COMPLETED(901),
+    @Deprecated PROCESSING(902),
+    ACTIVE(903),
+    MERGED(904),
+    PAUSED(910),
+    @Deprecated COMPLETE(911);
 
     private int code;
 
     ProjectStatusType(int code) {
         this.code = code;
-    }
-
-    public int getCode() {
-        return code;
     }
 
     public static ProjectStatusType getType(int code) {
