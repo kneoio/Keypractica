4be19ab3f0de1c304dcb29b1cae116c6320bedcc
kaira <justaidajam@gmail.com>
2024-06-23 22:21:50 +0100

    Commit Message:
    "Added Customization for ObjectMapper and ZonedDateTime Serialization
    
    - Created a new MapperCustomizer class to customize the ObjectMapper settings.
    - Adjusted ObjectMapper settings to not fail on empty beans and unknown properties.
    - Added a custom serializer for ZonedDateTime within the MapperCustomizer to format it to a specific pattern.
    - Introduced a new dependency 'jackson-datatype-jsr310' in pom.xml for handling java 8 date/time API.
    - Created a new CustomZonedDateTimeSerializer class to define serialization of ZonedDateTime."

--- core/src/main/java/io/kneo/core/server/mapper/MapperCustomizer.java
+++ core/src/main/java/io/kneo/core/server/mapper/MapperCustomizer.java
@@ -1,22 +1,30 @@
-package io.kneo.core.server;
+package io.kneo.core.server.mapper;
 
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.quarkus.jackson.ObjectMapperCustomizer;
 import jakarta.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.ZonedDateTime;
 
 
 @Singleton
 public class MapperCustomizer implements ObjectMapperCustomizer {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MapperCustomizer.class);
 
     @Override
     public void customize(ObjectMapper objectMapper) {
         objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-/*        SimpleModule module = new SimpleModule();
-        module.addDeserializer(LocalDateTime.class, new CustomLocalDateTimeDeserializer());
-        module.addSerializer(LocalDateTime.class, new CustomLocalDateTimeSerializer());
-        objectMapper.registerModule(module);*/
+        LOGGER.info("set ISO 8601 format");
+
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+        javaTimeModule.addSerializer(ZonedDateTime.class, new CustomZonedDateTimeSerializer());
+
+        objectMapper.registerModule(javaTimeModule);
     }
 }
