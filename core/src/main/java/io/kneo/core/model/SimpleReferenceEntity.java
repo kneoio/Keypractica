e921dff3a13a217943776990c9ee707f2cf0fafd
kaira <justaidajam@gmail.com>
2024-07-02 23:04:00 +0100

    Updated GitHub Actions workflow for Keypractica project, improved localization handling, and refactored organization controller.

--- core/src/main/java/io/kneo/core/model/SimpleReferenceEntity.java
+++ core/src/main/java/io/kneo/core/model/SimpleReferenceEntity.java
@@ -1,22 +1,35 @@
 package io.kneo.core.model;
 
 import io.kneo.core.localization.LanguageCode;
-import lombok.Getter;
+import io.kneo.core.server.Environment;
 import lombok.Setter;
 
 import java.util.EnumMap;
 import java.util.UUID;
 
-@Getter
 @Setter
 public class SimpleReferenceEntity extends DataEntity<UUID> {
     protected String identifier;
     protected EnumMap<LanguageCode, String> localizedName = new EnumMap<>(LanguageCode.class);
 
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public EnumMap<LanguageCode, String> getLocalizedName() {
+        for (LanguageCode code : Environment.AVAILABLE_LANGUAGES) {
+            if (!localizedName.containsKey(code)) {
+                localizedName.put(code, "");
+            }
+        }
+
+        return localizedName;
+    }
+
     public String getLocalizedName(LanguageCode lang) {
         try {
             String val = localizedName.get(lang);
-            if (val != null && (!val.isEmpty())) {
+            if (val!= null &&!val.isEmpty()) {
                 return val;
             } else {
                 return identifier;
@@ -25,5 +38,4 @@ public class SimpleReferenceEntity extends DataEntity<UUID> {
             return identifier;
         }
     }
-
 }
--- projects/src/main/java/io/kneo/projects/ModuleConst.java
+++ projects/src/main/java/io/kneo/projects/ModuleConst.java
@@ -1,11 +1,15 @@
 package io.kneo.projects;
 
 
+import io.kneo.core.localization.LanguageCode;
 import io.kneo.core.server.DefaultAppConst;
+
+import java.util.List;
 
 public class ModuleConst extends DefaultAppConst {
 	public static final String CODE = "prj";
 	public static final String NAME = "Projects";
+	public static List<LanguageCode> AVAILABLE_LANGUAGES = List.of(LanguageCode.ENG, LanguageCode.POR);
 	public static String NAME_ENG = "Projects";
 	public static String NAME_RUS = "Проекты";
 	public static String NAME_KAZ = "Жобалар";
