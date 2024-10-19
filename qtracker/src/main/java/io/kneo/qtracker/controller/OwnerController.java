fe33c395a95fb9df124a1406db266b6a5cd79319
kaira <justaidajam@gmail.com>
2024-10-19 23:08:31 +0100

    ####

--- qtracker/src/main/java/io/kneo/qtracker/controller/OwnerController.java
+++ qtracker/src/main/java/io/kneo/qtracker/controller/OwnerController.java
@@ -8,7 +8,6 @@ import io.kneo.core.dto.view.View;
 import io.kneo.core.dto.view.ViewPage;
 import io.kneo.core.localization.LanguageCode;
 import io.kneo.core.model.user.IUser;
-import io.kneo.core.repository.exception.DocumentHasNotFoundException;
 import io.kneo.core.repository.exception.DocumentModificationAccessException;
 import io.kneo.core.repository.exception.UserNotFoundException;
 import io.kneo.core.service.UserService;
@@ -64,10 +63,22 @@ public class OwnerController extends AbstractSecuredController<Owner, OwnerDTO> 
 
                     rc.response().setStatusCode(200).end(JsonObject.mapFrom(viewPage).encode());
                 },
-                failure -> {
-                    LOGGER.error("Error processing request: ", failure);
-                    rc.response().setStatusCode(500).end("Internal Server Error");
-                }
+                rc::fail
+        );
+    }
+
+    @Route(path = "/telegram/:telegram_id", methods = Route.HttpMethod.GET, produces = "application/json")
+    public void getByTelegramId(RoutingContext rc) throws UserNotFoundException {        ;
+        String id = rc.pathParam("telegram_id");
+        LanguageCode languageCode = LanguageCode.valueOf(rc.request().getParam("lang", LanguageCode.ENG.name()));
+
+        service.getDTOByTelegramId(id, getUser(rc), languageCode).subscribe().with(
+                owner -> {
+                    FormPage page = new FormPage();
+                    page.addPayload(PayloadType.DOC_DATA, owner);
+                    rc.response().setStatusCode(200).end(JsonObject.mapFrom(page).encode());
+                },
+                rc::fail
         );
     }
 
@@ -83,16 +94,28 @@ public class OwnerController extends AbstractSecuredController<Owner, OwnerDTO> 
                     page.addPayload(PayloadType.CONTEXT_ACTIONS, new ActionBox());
                     rc.response().setStatusCode(200).end(JsonObject.mapFrom(page).encode());
                 },
-                failure -> {
-                    if (failure instanceof DocumentHasNotFoundException) {
-                        rc.response().setStatusCode(404).end("Owner not found");
-                    } else {
-                        LOGGER.error("Error processing request: ", failure);
-                        rc.response().setStatusCode(500).end("Internal Server Error");
-                    }
-                }
+                rc::fail
         );
     }
+
+
+    @Route(path = "/:messenger_type/:id?", methods = Route.HttpMethod.POST, consumes = "application/json", produces = "application/json")
+    public void upsertMessengerUser(RoutingContext rc) throws UserNotFoundException, DocumentModificationAccessException {
+        String id = rc.pathParam("id");
+        String messengerType = rc.pathParam("messenger_type");
+        JsonObject jsonObject = rc.body().asJsonObject();
+        OwnerDTO dto = jsonObject.mapTo(OwnerDTO.class);
+        service.upsert(id, dto, getUser(rc), LanguageCode.ENG)
+                .subscribe().with(
+                        doc -> {
+                            int statusCode = id == null ? 201 : 200;
+                            rc.response().setStatusCode(statusCode).end(JsonObject.mapFrom(doc).encode());
+                        },
+                        rc::fail
+                );
+    }
+
+/*
 
     @Route(path = "/:id?", methods = Route.HttpMethod.POST, consumes = "application/json", produces = "application/json")
     public void upsert(RoutingContext rc) throws UserNotFoundException, DocumentModificationAccessException {
@@ -108,20 +131,15 @@ public class OwnerController extends AbstractSecuredController<Owner, OwnerDTO> 
                         rc::fail
                 );
     }
+*/
+
 
     @Route(path = "/:id", methods = Route.HttpMethod.DELETE, produces = "application/json")
-    public void delete(RoutingContext rc) {
-        try {
-            String id = rc.pathParam("id");
-            service.delete(id, getUser(rc)).subscribe().with(
-                    count -> rc.response().setStatusCode(count > 0 ? 204 : 404).end(),
-                    failure -> {
-                        LOGGER.error(failure.getMessage(), failure);
-                        rc.response().setStatusCode(500).end("Internal Server Error");
-                    }
-            );
-        } catch (UserNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+    public void delete(RoutingContext rc) throws UserNotFoundException {
+        String id = rc.pathParam("id");
+        service.delete(id, getUser(rc)).subscribe().with(
+                count -> rc.response().setStatusCode(count > 0 ? 204 : 404).end(),
+                rc::fail
+        );
     }
 }
